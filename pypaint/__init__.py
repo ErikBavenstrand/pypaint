42c6a857c841f5759dce6a6877662c18573ceaca
Erik Båvenstrand <erik.bavenstrands@gmail.com>
2018-12-14 13:45:12 +0100

    initial

---
+++ pypaint/__init__.py
@@ -0,0 +1,7 @@
+from turtle import color, begin_fill, forward, left, pos, end_fill, done, hideturtle, speed, delay, update, tracer
+
+hideturtle()
+speed(0)
+delay(0)
+tracer(0, 0)
+done()
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='pypaint',
+      version='0.1',
+      description='Drawing module for python',
+      url='',
+      author='Erik Båvenstrand',
+      author_email='erik.bavenstrands@gmail.com',
+      license='MIT',
+      packages=['pypaint'],
+      install_requires=['turtle'],
+      zip_safe=False)
